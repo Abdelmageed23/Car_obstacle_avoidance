e317ce0ced0d13c137c42e185fbdb9f9c524f842
Abdelmageed23 <53024894+Abdelmageed23@users.noreply.github.com>
2022-02-12 14:52:51 +0200

    APP V1.0

--- APP/APP.h
+++ APP/APP.h
@@ -1,7 +1,19 @@
 #ifndef APP_H_
 #define APP_H_
 
+
+
 void APP_vidInit(void);
 void APP_vidStart(void);
+
+
+ERROR_STATUS_t Robot_Init();
+ERROR_STATUS_t Robot_Move(uint8_t Direction, uint8_t Speed);
+ERROR_STATUS_t Robot_Stop();
+
+#define FORWARD 1
+#define BACKWORD 2
+#define RIGHT	3
+#define LEFT 4
 
 #endif
