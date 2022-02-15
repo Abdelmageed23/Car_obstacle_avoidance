d6e504131b01945157c8f9169e533799b34ce3ea
M0oAlaa <96453780+M0oAlaa@users.noreply.github.com>
2022-02-15 11:53:17 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,45 +1,17 @@
-#include "LIB/STD_TYPES.h"
 #include "APP/APP.h"
-#include "HAL/LCD/LCD.h"
-#include "HAL/ULTRASONIC/ULTRASONIC_interface.h"
-#include "MCAL/DELAY/Delay.h"
-#include "MCAL/Interrupt/Interrupt_interface.h"
-#include "util/delay.h"
 
 
 int main()
 {
+	/*Call App initialization*/
     APP_vidInit();
+
+    /*Start Application*/
     APP_vidStart();
 
     return 0;
 
 }
 
-/*
-void main()
-{
-	DELAY_init();
-	GlobalInerruptEnable();
-	ULTRASONIC_init();
-	uint8_t x=0;
-		while(1)
-			{
-				LCD_Init();
-				if(ULTRASONIC_GetDis(&x)==E_OK)
-				{
-					LCD_SetPosition(1,0);
-					LCD_WriteString("          ");
-					LCD_SetPosition(1,0);
-					LCD_WriteNumber(x);
 
-				}
-				_delay_ms(500);
-//				LCD_SetPosition(1,0);
-//				LCD_WriteNumber(23);
-				//LCD_WriteString("HHHHHHHHHH");
-			}
-}
-
-*/
 
